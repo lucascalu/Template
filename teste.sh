aa5182483e33f87c55dee965b2117ef3e4154ced
Lucas Schiavon Calu <54896910+lucascalu@users.noreply.github.com>
2024-02-05 09:59:33 -0300

    Add files via upload

---
+++ teste.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "vamos iniciar as atividades"
+ sudo apt update
+ sudo apt upgrade
+ sudo apt install ssh
+ sudo apt install apache2
+ sudo apt install php
+
+echo "atividade concluida"
